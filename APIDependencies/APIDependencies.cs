92b044f214f6a05e31cba996b79f600f35a351fa
uGuardian <Sean.Heavysege@gmail.com>
2020-08-15 11:54:12 -0600

    Initial Commit

---
+++ APIDependencies/APIDependencies.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+
+namespace APIDependencies
+{
+    [BepInDependency("com.bepis.r2api")]
+
+    [BepInPlugin(
+        "com.uGuardian.APIDependencies",
+        "APIDependencies",
+        "1.0.0")]
+
+    [R2APISubmoduleDependency(("ItemAPI"),("ItemDropAPI"),("AssetPlus"),("AssetAPI"),("Utils.CommandHelper"),("DifficultyAPI"),("EntityAPI"),("InventoryAPI"),("LoadoutAPI"),("LobbyConfigAPI"),("PlayerAPI"),("ResourcesAPI"),("SurvivorAPI"),("SkinAPI"),("SkillAPI"))]
+    public class APIDependencies : BaseUnityPlugin
+    {
+        public void Awake() { Chat.AddMessage("APIDependencies stopgap fix loaded"); }
+    }
+}
---
+++ APIDependencies/README.md
@@ -0,0 +1,2 @@
+# APIDependencies
+TODO
