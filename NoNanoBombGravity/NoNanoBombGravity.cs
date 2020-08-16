c29e8394b7b13ace20bc92bea87b9e56311db76e
uGuardian <Sean.Heavysege@gmail.com>
2020-08-15 23:59:07 -0600

    NoNanoBombGravity 1.0.0 commit

---
+++ NoNanoBombGravity/NoNanoBombGravity.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+
+
+namespace NoNanoBombGravity
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin(
+        "com.uGuardian.NoNanoBombGravity",
+        "NoNanoBombGravity",
+        "1.0.0")]
+
+    public class NoNanoBombGravity:BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.AntiGravityForce.FixedUpdate += (orig, self) =>
+            {
+                self.antiGravityCoefficient = 1;
+                orig(self);
+            };
+        }
+    }
+}
---
+++ NoNanoBombGravity/README.md
@@ -0,0 +1,7 @@
+# NoNanoBombGravity
+Turns off gravity for Artificer's Nano Bomb.
+
+Made at the request of Chum.
+
+Currently does this by directly overriding AntiGravityForce.antiGravityCoefficient, and thusly will impact any other mods that call the monobehavior.
+To the best of my knowledge Nano Bomb is the only thing in the game that actually uses this script, please @uGuardian on the modding Discord if you encounter any problems with this mod or have any suggestions!
