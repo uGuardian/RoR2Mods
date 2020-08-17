bad10e9d6cb1e329b5c1320bd5e7235b5f5042eb
uGuardian <Sean.Heavysege@gmail.com>
2020-08-16 23:35:49 -0600

    NoEnigmaRecycler 1.0.0 commit

---
+++ NoEnigmaRecycler/NoEnigmaRecycler.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using RoR2;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+
+namespace NoEnigmaRecycler
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin(
+        "com.uGuardian.NoEnigmaRecycler",
+        "NoEnigmaRecycler",
+        "1.0.0")]
+
+    public class NoEnigmaRecycler:BaseUnityPlugin
+    {
+
+        public void Awake()
+        {
+            IL.RoR2.EquipmentCatalog.Init += il =>
+            {
+                var c = new ILCursor(il);
+
+                c.GotoNext(
+                    x => x.MatchLdstr("EQUIPMENT_RECYCLER_NAME"),
+                    x => x.MatchStfld<EquipmentDef>("nameToken"),
+                    x => x.MatchDup()
+                );
+                c.GotoNext(
+                    x => x.MatchDup(),
+                    x => x.MatchLdcI4(1),
+                    x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
+                );
+
+                c.Index += 1;
+                c.RemoveRange(1);
+                c.Emit(OpCodes.Ldc_I4, 0);
+
+            };
+
+        }  
+    }
+}
