20caedde5409faf0903e3ae77c715efe25382353
uGuardian <Sean.Heavysege@gmail.com>
2020-08-16 19:53:12 -0600

    Added configuration option

--- NoNanoBombGravity/NoNanoBombGravity.cs
+++ NoNanoBombGravity/NoNanoBombGravity.cs
@@ -11,15 +11,24 @@ namespace NoNanoBombGravity
     [BepInPlugin(
         "com.uGuardian.NoNanoBombGravity",
         "NoNanoBombGravity",
-        "1.0.0")]
+        "1.1.0")]
 
     public class NoNanoBombGravity:BaseUnityPlugin
     {
+        public static BepInEx.Configuration.ConfigEntry<float> antiGravitySetting { get; set; }
+
         public void Awake()
         {
+            antiGravitySetting = Config.Bind<float>(
+				"NoNanoBombGravity",
+				"antiGravityCoefficient",
+				1,
+				"Changes how much Nano Bomb resists gravity. Mod default is 1, vanilla default is 0.7"
+			);
+
             On.RoR2.AntiGravityForce.FixedUpdate += (orig, self) =>
             {
-                self.antiGravityCoefficient = 1;
+                self.antiGravityCoefficient = antiGravitySetting.Value;
                 orig(self);
             };
         }
