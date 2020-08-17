0edd8dd6ec209d722bc6502dd1ef9ff11d0c359d
uGuardian <Sean.Heavysege@gmail.com>
2020-08-17 09:34:00 -0600

    NoEnigmaRecycler Version 2.0.0

--- NoEnigmaRecycler/NoEnigmaRecycler.cs
+++ NoEnigmaRecycler/NoEnigmaRecycler.cs
@@ -10,34 +10,140 @@ namespace NoEnigmaRecycler
     [BepInPlugin(
         "com.uGuardian.NoEnigmaRecycler",
         "NoEnigmaRecycler",
-        "1.0.0")]
+        "2.0.0")]
 
     public class NoEnigmaRecycler:BaseUnityPlugin
     {
+        public static BepInEx.Configuration.ConfigEntry<bool> Recycler { get; set; }
+        public static BepInEx.Configuration.ConfigEntry<bool> Lightning { get; set; }
+        public static BepInEx.Configuration.ConfigEntry<bool> LunarPotion { get; set; }
+        public static BepInEx.Configuration.ConfigEntry<bool> LunarMeteor { get; set; }
+        public static BepInEx.Configuration.ConfigEntry<bool> LunarHellfire { get; set; }
 
         public void Awake()
         {
-            IL.RoR2.EquipmentCatalog.Init += il =>
-            {
+            Recycler = Config.Bind<bool>(
+				"Normal",
+				"Recycler",
+				false,
+				"True if Enigma can roll this item"
+			);
+            Lightning = Config.Bind<bool>(
+				"Normal",
+				"Royal Capacitor",
+				true,
+				"True if Enigma can roll this item"
+			);
+            LunarPotion = Config.Bind<bool>(
+				"Lunar",
+				"Spinel Tonic",
+				true,
+				"True if Enigma can roll this item"
+			);
+            LunarMeteor = Config.Bind<bool>(
+				"Lunar",
+				"Glowing Meteorite",
+				true,
+				"True if Enigma can roll this item"
+			);
+            LunarHellfire = Config.Bind<bool>(
+				"Lunar",
+				"Helfire Tincture",
+				true,
+				"True if Enigma can roll this item"
+			);
+
+            IL.RoR2.EquipmentCatalog.Init += il => {
+
                 var c = new ILCursor(il);
 
-                c.GotoNext(
-                    x => x.MatchLdstr("EQUIPMENT_RECYCLER_NAME"),
-                    x => x.MatchStfld<EquipmentDef>("nameToken"),
-                    x => x.MatchDup()
-                );
-                c.GotoNext(
-                    x => x.MatchDup(),
-                    x => x.MatchLdcI4(1),
-                    x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
-                );
+                if (!Recycler.Value) {
+                    c.GotoNext(
+                        x => x.MatchLdstr("EQUIPMENT_RECYCLER_NAME"),
+                        x => x.MatchStfld<EquipmentDef>("nameToken"),
+                        x => x.MatchDup()
+                    );
+                    c.GotoNext(
+                        x => x.MatchDup(),
+                        x => x.MatchLdcI4(1),
+                        x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
+                    );
+                    c.Index += 1;
+                    c.RemoveRange(1);
+                    c.Emit(OpCodes.Ldc_I4, 0);
+                    c.Index = 0;
+                }
 
-                c.Index += 1;
-                c.RemoveRange(1);
-                c.Emit(OpCodes.Ldc_I4, 0);
+                if (!Lightning.Value) {
+                    c.GotoNext(
+                        x => x.MatchLdstr("EQUIPMENT_LIGHTNING_NAME"),
+                        x => x.MatchStfld<EquipmentDef>("nameToken"),
+                        x => x.MatchDup()
+                    );
+                    c.GotoNext(
+                        x => x.MatchDup(),
+                        x => x.MatchLdcI4(1),
+                        x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
+                    );
+                    c.Index += 1;
+                    c.RemoveRange(1);
+                    c.Emit(OpCodes.Ldc_I4, 0);
+                    c.Index = 0;
+                }
 
+                if (!LunarPotion.Value) {
+                    //Tonic is weird, the equipment LunarPotion has the name, but the actual drop is the item Tonic.
+                    c.GotoNext(
+                        //EquipmentIndex.Tonic has no name, so we need to refer to something else.
+                        x => x.MatchLdstr("Prefabs/PickupModels/PickupTonic"),
+                        x => x.MatchStfld<EquipmentDef>("pickupModelPath"),
+                        x => x.MatchDup()
+                    );
+                    c.GotoNext(
+                        x => x.MatchDup(),
+                        x => x.MatchLdcI4(1),
+                        x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
+                    );
+                    c.Index += 1;
+                    c.RemoveRange(1);
+                    c.Emit(OpCodes.Ldc_I4, 0);
+                    c.Index = 0;
+                }
+                
+                if (!LunarMeteor.Value) {
+                    c.GotoNext(
+                        x => x.MatchLdstr("EQUIPMENT_METEOR_NAME"),
+                        x => x.MatchStfld<EquipmentDef>("nameToken"),
+                        x => x.MatchDup()
+                    );
+                    c.GotoNext(
+                        x => x.MatchDup(),
+                        x => x.MatchLdcI4(1),
+                        x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
+                    );
+                    c.Index += 1;
+                    c.RemoveRange(1);
+                    c.Emit(OpCodes.Ldc_I4, 0);
+                    c.Index = 0;
+                }
+
+                if (!LunarHellfire.Value) {
+                    c.GotoNext(
+                        x => x.MatchLdstr("EQUIPMENT_BURNNEARBY_NAME"),
+                        x => x.MatchStfld<EquipmentDef>("nameToken"),
+                        x => x.MatchDup()
+                    );
+                    c.GotoNext(
+                        x => x.MatchDup(),
+                        x => x.MatchLdcI4(1),
+                        x => x.MatchStfld<EquipmentDef>("enigmaCompatible")
+                    );
+                    c.Index += 1;
+                    c.RemoveRange(1);
+                    c.Emit(OpCodes.Ldc_I4, 0);
+                    c.Index = 0;
+                }
             };
-
         }  
     }
 }
--- NoEnigmaRecycler/README.md
+++ NoEnigmaRecycler/README.md
@@ -1,10 +1,15 @@
 # NoEnigmaRecycler
-Enigma will never change to Recycler.
+Enigma will never change to disabled items.
+Only Recycler by default.
 
 Have you ever had an amusing chain of items going in Enigma, only to be stalled by Recycler and a lack of nearby items? Well no more!
 You will still get a target requirement break with royal capacitor though, this is probably a good thing whenever gesture of the drowned gets involved.
 
-I may consider making a config to also disable Royal Capacitor, and possibly lunar items in the future.
-
-Uses IL searching to make EnigmaCompatible for Recycler in the item catalog false.
+Uses IL searching to make EnigmaCompatible for the given item in the item catalog false.
 Please @uGuardian on the modding Discord if you encounter any problems with this mod or have any suggestions!
+
+### Changelog
+
+#### 2.0.0:
+Configuration has been added, now includes all lunar items (execpt effigy, which is already disabled in vanilla) and Royal Capacitor!
+To change this, go into the config and set the item of choice to "false"
